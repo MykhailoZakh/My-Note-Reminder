b743320339186ab4200594a4aa671dbce6304972
Mykhailo <sharkon90@gmail.com>
2023-12-27 14:14:24 -0500

    adding ger fucntion for main screen and /notes screen

---
+++ server.js
@@ -0,0 +1,22 @@
+// Initializing express and store it in the variable
+const express = require('express');
+const path = require('path');
+const PORT = 3001;
+const app = express();
+// Import api route file
+const api = require()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+//  GET function for main screen http://localhost:3001
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+//  GET function for http://localhost:3001/notes end
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+// server listenner when user trying to get http://localhost:3001 
+app.listen(PORT, () =>
+    console.log(`app listennig : http://localhost:${PORT}`));
